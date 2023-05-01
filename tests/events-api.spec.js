bd97de8b6bf0edfd76533f3d29c9a790562360a7
AliA <abdelazim.ali@integrant.com>
2023-05-01 17:59:18 +0300

    fix(test): clean db after test and use connection string as standalone

--- tests/events-api.spec.js
+++ tests/events-api.spec.js
@@ -1,25 +1,35 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+import db from '../data/database.js';
 
-test('event creation', async ({ request }) => {
-  const testTitle = 'Test event';
-  const response = await request.post('/', {
-    data: {
-      title: testTitle,
-    },
+test.describe.configure({ mode: 'serial' });
+test.describe('CRUD Operation', () => {
+  test('event creation', async ({ request }) => {
+    const testTitle = 'Test event';
+    const response = await request.post('/', {
+      data: {
+        title: testTitle,
+      },
+    });
+    expect(response.ok()).toBeTruthy();
+    const resDataRaw = await response.body();
+    const resData = JSON.parse(resDataRaw.toString());
+    expect(resData).toHaveProperty('event.id');
+    expect(resData.event.title).toBe(testTitle);
   });
-  expect(response.ok()).toBeTruthy();
-  const resDataRaw = await response.body();
-  const resData = JSON.parse(resDataRaw.toString());
-  expect(resData).toHaveProperty('event.id');
-  expect(resData.event.title).toBe(testTitle);
-});
 
-test('getting events', async ({ request }) => {
-  const response = await request.get('/');
-  expect(response.ok()).toBeTruthy();
-  const resDataRaw = await response.body();
-  const resData = JSON.parse(resDataRaw.toString());
-  expect(resData).toHaveProperty('events');
-  expect(resData.events.length).toBeGreaterThan(0);
+  test('getting events', async ({ request }) => {
+    const response = await request.get('/');
+    expect(response.ok()).toBeTruthy();
+    const resDataRaw = await response.body();
+    const resData = JSON.parse(resDataRaw.toString());
+    expect(resData).toHaveProperty('events');
+    expect(resData.events.length).toBeGreaterThan(0);
+  });
+
+  test.afterAll(async () => {
+    console.log('Done with tests: Start Cleaning Up');
+    await db.collection('events').deleteMany({ title: 'Test event' });
+  });
+
 });
